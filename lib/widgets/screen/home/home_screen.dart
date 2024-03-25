e0d7c224cd2e890916382af28c318e3779793bb8
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2024-03-25 08:25:09 +0300

    feat: update home screen

--- lib/widgets/screen/home/home_screen.dart
+++ lib/widgets/screen/home/home_screen.dart
@@ -1,14 +1,14 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
-import '../my_profile/my_profile_screen.dart';
 import '/core/router/router.dart';
 import '/core/style/icons.dart';
 import 'widget/bottom_navigation_bar_item.dart';
 
+
 @RoutePage()
 class HomeShopScreen extends StatefulWidget {
-  const HomeShopScreen({super.key});
+  const HomeShopScreen({ super.key, });
 
   @override
   State<HomeShopScreen> createState() => _HomeShopScreenState();
@@ -16,37 +16,40 @@ class HomeShopScreen extends StatefulWidget {
 
 class _HomeShopScreenState extends State<HomeShopScreen> {
   @override
-  Widget build(BuildContext context) => AutoTabsScaffold(
-    routes: const [
-      FeedRoute(),
-      AddMemeRoute(),
-      MyProfileRoute(),
-    ],
-    bottomNavigationBuilder: (_, tabsRouter) => BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      currentIndex: tabsRouter.activeIndex,
-      onTap: tabsRouter.setActiveIndex,
-      selectedItemColor: Theme.of(context).primaryColor,
-      unselectedItemColor: Theme.of(context).unselectedWidgetColor,
-      selectedLabelStyle: Theme.of(context).textTheme.labelMedium,
-      unselectedLabelStyle: Theme.of(context).textTheme.labelMedium,
-      items: [
-        CustomBottomNavigationBarItem.build(
-          iconHome,
-          'Лента',
-          context,
-        ),
-        CustomBottomNavigationBarItem.build(
-          iconAdd,
-          'Добавить',
-          context,
-        ),
-        CustomBottomNavigationBarItem.build(
-          iconProfile,
-          'Профиль',
-          context,
-        ),
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      leading: const AutoLeadingButton(showIfChildCanPop: false),
+      title: const Text('Meme Store'),
+    ),
+    body: AutoTabsScaffold(
+      routes: const [
+        FeedRoute(),
+        AddMemeRoute(),
+        MyProfileRoute(),
       ],
+      bottomNavigationBuilder: (_, tabsRouter) => BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: tabsRouter.activeIndex,
+        onTap: tabsRouter.setActiveIndex,
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Theme.of(context).unselectedWidgetColor,
+        selectedLabelStyle: Theme.of(context).textTheme.labelMedium,
+        unselectedLabelStyle: Theme.of(context).textTheme.labelMedium,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Feed',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'New meme',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'My profile',
+          ),
+        ],
+      ),
     ),
   );
 }
